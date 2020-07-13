1180f4446bfc60129416ab4adef91369da3e8410
Anton Bartoshevich <toni.bartoshevich@mail.ru>
2020-07-13 23:06:44 +0300

    Напишите if, аналогичный switch

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
-let num = prompt('Введите число большее 100');
-for (let i = 0; i <= 100; i++) {
+let browser = prompt('Which use a browser ?',"");
+    if (browser == 'Edge') {
+    alert("You've got the Edge!");
+    
 
-    if (num < 100) {
-        num = prompt('Введите ещё раз число большее 100');
-    }   
-    else {
-        alert('Бинго!');
-        break;
-    } 
 }
+    else if (browser == 'Chrome' && browser == 'Firefox' && 
+         browser == 'Safari' && browser == 'Opera') {
+            
+            alert('Okay we support these browsers too');
+            
+}
+    else {
+        alert('We hope that this page looks ok!');
+        
+    }
 
+
+
+
+
+
+
+
+// switch (browser) {
+//     case 'Edge':
+//       alert( "You've got the Edge!" );
+//       break;
+  
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari':
+//     case 'Opera':
+//       alert( 'Okay we support these browsers too' );
+//       break;
+  
+//     default:
+//       alert( 'We hope that this page looks ok!' );
+//   }
 
 
 
