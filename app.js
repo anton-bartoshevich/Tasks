03e7a8810968bc73837ab903dfc7f49e7aeaf08f
Anton Bartoshevich <toni.bartoshevich@mail.ru>
2020-07-14 20:10:28 +0300

    Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  };
+  let sum = 0;
+  for (let key in salaries) {
+      sum = salaries[key1] + salaries[key2] + salaries[key3];
+      }
+  alert(sum);
 
-let isEmpty = function(object) {
-	object = {
-   name:"John",
-   age:24
-   };
-   }
-   alert(isEmpty());
+	
    
    
 
