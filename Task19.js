66feeedc07f92897a7f851ac96acab509700fe98
Anton-bartoshevich <Toni.bartoshevich@mail.ru>
2020-07-16 22:59:35 +0300

    Task19

---
+++ Task19.js
@@ -0,0 +1,12 @@
+/*Создайте функцию multiplyNumeric(obj), которая умножает все числовые …
+…свойства объекта obj на 2 */
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  };
+  let sum = 0;
+  for (let key in salaries) {
+      sum = salaries[key1] + salaries[key2] + salaries[key3];
+      }
+  alert(sum);
